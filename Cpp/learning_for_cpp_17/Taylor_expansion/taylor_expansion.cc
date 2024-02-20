c3f449d122218c17f086306a6e3cfcc485791f2b
Zichen <zichen.li@dalembert.upmc.fr>
2024-02-20 18:18:18 +0100

    Add Cooley-Tukey FFT notebook and Taylor expansion code

--- Cpp/learning_for_cpp_17/Taylor_expansion/taylor_expansion.cc
+++ Cpp/learning_for_cpp_17/Taylor_expansion/taylor_expansion.cc
@@ -43,12 +43,54 @@ double taylor_exp_n(double x, int N)
     double res = 1.;
     double a = 1.;
     N++;
-    //
+    //store a bool whether x is negative or not
     bool neg = (x < 0.);
-    //
+    //take the absolute value of x
+    double y = neg ? -x : x;
+    //same computation as before taylor_expansion
     for (int i = 1; i < N; ++i) {
-        a *= x / i;
+        a *= y / i;
         res += a;
     }
+    //if x is negative, the result is the inverse of the result for the absolute value of x
+    // => res=e^y=e^-x=(e^x)^-1=1./e^x
+    // => exp(-x) = 1./exp(x)
+    if (neg) res = 1./res;
+    
     return res;
+}
+
+int main(int argc, char *argv[]) 
+{
+    //variable declaration
+    double xmin, xmax, dx;
+    int Order, nbpoints;
+    // entering user input
+    cout <<  "Give the range of x (xmin, xmax): ";
+    cin >> xmin >> xmax;
+    cout <<  "Give the number of points(not counting boundary points): "<<
+    endl;
+    cin >> nbpoints;
+    nbpoints += 2;
+    cout <<  "Give the order of the expansion: "<<endl;
+    cin >> Order;
+
+    //compute the step size
+    dx = (xmax - xmin) / (nbpoints - 1);
+    //loop over the range of x
+    cout << "Taylor expansion of exp(x) from " << xmin << " to " << xmax << " with " << Order << " terms" << endl;
+    for (int i = 0; i < nbpoints; ++i) {
+        double x = xmin + i * dx;
+        cout << "x " << i << " : " << taylor_expansion(x, Order) << endl;
+    }
+    cout << endl;
+
+    //loop over the range of x to compute exact exponential value, the asked function, the recursive implementation and the negative implementation
+    cout << "Comparison"<< endl;
+    for (int i = 0; i < nbpoints; ++i) {
+        double x = xmin + i * dx;
+        cout << scientific << x << " : " << exp(x) << " : " << taylor_expansion(x, Order) << " : " << taylor_exp_r(x, Order) << " : " << taylor_exp_n(x, Order) << endl;
+    }
+
+    return 0;
 }
