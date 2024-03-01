73917b86017d06f557c1b32aa7bbb4b4fb7e8386
Zichen <zichen.li@dalembert.upmc.fr>
2024-03-01 11:24:48 +0100

    Implement matrix class methods

--- Week5/sover_develop_workflow/matrix.h
+++ Week5/sover_develop_workflow/matrix.h
@@ -19,10 +19,37 @@ class matrix {
         int getCols() const;
 };
 
+//implementation of the class
+template<typename T>
+matrix<T>::matrix(int rows, int cols) : rows(rows_), cols(cols_) {
+    data = new T[rows * cols];
+}
 
+template<typename T>
+matrix<T>::~matrix() {
+    delete[] data;
+}
 
+template<typename T>
+T& matrix<T>::operator()(int i, int j) {
+    int index = i * cols + j;
+    return data[index];
+}
 
+template<typename T>
+T matrix<T>::operator()(int i, int j) const {
+    int index = i * cols + j;
+    return data[index];
+}
 
+template<typename T>
+int matrix<T>::getRows() const {
+    return rows;
+}
 
+template<typename T>
+int matrix<T>::getCols() const {
+    return cols;
+}
 
 #endif // MATRIX_H
