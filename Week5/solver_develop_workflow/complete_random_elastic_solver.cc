28e215872105a7c8068de11a64a93017e37f71de
Zichen <zichen.li@dalembert.upmc.fr>
2024-03-11 15:05:47 +0100

    Refactor code and add header file for complete_random_elastic_solver

--- Week5/solver_develop_workflow/complete_random_elastic_solver.cc
+++ Week5/solver_develop_workflow/complete_random_elastic_solver.cc
@@ -8,56 +8,7 @@
 #include </gagarine/temporaires/zli/eigen-3.4.0/Eigen/Dense>
 #include <random> // for std::random_device, std::mt19937, std::normal_distribution
 #include <fstream> // for std::ofstream
-
-
-// Function to generate meshgrid equivalent in C++
-void GenerateMeshgrid(Eigen::VectorXd& x, Eigen::VectorXd& y,Eigen::MatrixXd& X, Eigen::MatrixXd& Y, double L, int n, int m);
-
-// Function to realize numpy.fft.fftfreq in  C++
-Eigen::VectorXd fftfreq(int n, double d = 1.0);
-
-// Function to generate the meshgrid in Fourier space
-void GenerateFrequencyMeshgrid(Eigen::VectorXd& q_x, Eigen::VectorXd& q_y,Eigen::MatrixXd& Q_x, Eigen::MatrixXd& Q_y, double L, int n, int m);
-
-// Function to define the q value
-Eigen::MatrixXd computeQValues(const Eigen::VectorXd& Q_x, const Eigen::VectorXd& Q_y);
-
-// Function to define the piecewise function phi(q)
-Eigen::MatrixXd phi(const Eigen::MatrixXd& q, double phi_0, double q_l, double q_r, double q_s, double H);
-
-//Function to Generate white noise
-Eigen::MatrixXd generateWhiteNoise(int rows, int cols);
-
-// Function to generate random phase and white noise
-Eigen::MatrixXd generateRandomSurface(const Eigen::MatrixXd& phiValues, int n, int m);
-
-// Function to save data to a file
-void SaveSurfaceToFile(const Eigen::MatrixXd& surface, const std::string& filename);
-
-// Function to compute the mean of a vector
-template<typename T>
-double mean(const std::vector<T>& v);
-
-
-// Function to compute the sign of a vector
-/*
-template<typename T>
-std::vector<int> sign(const std::vector<T>& v);
-*/
-
-int sign(double value);
-
-// Function to define the alpha value
-double alphavalue(double alpha, const Eigen::MatrixXd& P, double W, double S);
-
-// Function to find the alpha value for steepest descent algorithm energy minimization
-double findAlpha0(const Eigen::MatrixXd& P, double W, double alpha_l, double alpha_r, double tol, double S);
-
-// Generate kernel function
-Eigen::MatrixXd generateKernel(const Eigen::MatrixXd& QX, const Eigen::MatrixXd& QY, double E_star);
-
-// Function to minimize the energy
-Eigen::MatrixXd computeDisplacment(const Eigen::MatrixXd& surface, Eigen::MatrixXd& P, Eigen::MatrixXd kernel_fourier, double W, double S, double error, double tol, int k, int maxIter);
+#include "complete_random_elastic_solver.h"
 
 
 ////////////////////////////////////////
@@ -311,15 +262,6 @@ Eigen::MatrixXd computeDisplacment(const Eigen::MatrixXd& surface, Eigen::Matrix
     fftw_plan p_forward = fftw_plan_dft_r2c_2d(n, m, P.data(), P_fourier, FFTW_ESTIMATE);
     fftw_plan p_backward = fftw_plan_dft_c2r_2d(n, m, G_fourier, G.data(), FFTW_ESTIMATE);
 
-
-    //Adapted Kernel, only contains (n*m/2+1) to match the size of P_
-
-
-    //Adapted Kernel
-
-
-
-
     while (std::abs(error) > tol && k < maxIter) {
 
 
@@ -449,13 +391,3 @@ int main(){
     return 0;
 
 }
-
-
-/*
-(base) zli@dionysos:~/Master-internship-repo-at-Institut-dAlembert/build$ make
-Consolidate compiler generated dependencies of target solver_cpp
-[100%] Built target solver_cpp
-(base) zli@dionysos:~/Master-internship-repo-at-Institut-dAlembert/build$ ./solver_cpp 
-solver_cpp: /gagarine/temporaires/zli/eigen-3.4.0/Eigen/src/Core/PlainObjectBase.h:366: void Eigen::PlainObjectBase<Derived>::resizeLike(const Eigen::EigenBase<OtherDerived>&) [with OtherDerived = Eigen::Matrix<double, -1, -1>; Derived = Eigen::Matrix<double, -1, 1>]: Assertion `other.rows() == 1 || other.cols() == 1' failed.
-Abandon (core dumped)
-*/
