0993aa67eb8c13d466047d0fbbef1dcf1e85799b
Zichen <zichen.li@dalembert.upmc.fr>
2024-03-18 13:58:24 +0100

    Add backward_euler.py and Representation of the Standard Linear Solid.jpg

---
+++ Week8/backward_euler.py
@@ -0,0 +1,27 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+
+
+
+
+
+
+
+
+#define input parameters
+##time
+t0 = 0
+t1 = 1
+dt = (t1 - t0)/1000
+##load(constant)
+W = 1000
+##spring constant
+Ginf = 1000
+
+
+
+
+
+
