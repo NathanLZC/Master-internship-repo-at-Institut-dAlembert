75ddf4b8a6ee04339a6001c3b71c0dba4a32846a
Zichen <zichen.li@eleves.ec-nantes.fr>
2024-05-02 15:33:23 +0200

    Add MaxwellViscoelastic class to Week14/maxwell_viscoelastic.hh

---
+++ Week14/maxwell_viscoelastic.hh
@@ -0,0 +1,50 @@
+/*
+ *  SPDX-License-Indentifier: AGPL-3.0-or-later
+ *
+ *  Copyright (©) 2016-2024 EPFL (École Polytechnique Fédérale de Lausanne),
+ *  Laboratory (LSMS - Laboratoire de Simulation en Mécanique des Solides),
+ *  Laboratory (IJLRDA - Institut Jean Le Rond d'Alembert)
+ *  Copyright (©) 2020-2024 Lucas Frérot
+ *
+ *  Part of codes in this script is insipred by Copilot and GPT-4.
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as published
+ *  by the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ */
+/* -------------------------------------------------------------------------- */
+#ifndef MAXWELL_VISCOELASTIC_HH
+#define MAXWELL_VISCOELASTIC_HH
+/* -------------------------------------------------------------------------- */
+#include "contact_solver.hh"
+#include "grid_view.hh"
+#include "meta_functional.hh"
+#include "westergaard.hh"
+/* -------------------------------------------------------------------------- */
+namespace tamaas {
+
+class MaxwellViscoelastic : public PolonskyKeerRey {
+public:
+  /// Types of algorithm (primal/dual) or constraint
+  enum type { gap, pressure };
+
+public:
+  /// Constructor
+  MaxwellViscoelastic(Model& model, const GridBase<Real>& surface,
+                      Real tolerance, type variable_type, type constraint_type,
+                      Real viscosity, Real time_step,
+                      std::vector<Real> shear_modulus,
+                      std::vector<Real> characteristic_time);
+  ~MaxwellViscoelastic() override = default;
+
+#endif  // MAXWELL_VISCOELASTIC_HH
